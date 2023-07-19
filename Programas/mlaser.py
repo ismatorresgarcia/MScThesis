22bccb52e35cc1e28e2756b51edff8d7cb88f337
Ismael <126611721+glabu@users.noreply.github.com>
2023-07-19 20:26:19 +0200

    19jul

--- Programas/mlaser.py
+++ Programas/mlaser.py
@@ -17,7 +17,6 @@ fig.scene.parallel_projection = True
 
 u = np.linspace(0, 2*np.pi, 1000)
 v = np.linspace(-8, 8, 1000)
-
 x = 2.5 * np.exp(-v**2 / 10) * np.sin(20*u)
 y = 2.5 * np.exp(-v**2 / 10) * np.cos(20*u)
 z = v
@@ -29,14 +28,14 @@ x = 2.48 * np.exp(-v**2 / 10) * np.sin(20*u)
 y = 2.48 * np.exp(-v**2 / 10) * np.cos(20*u)
 z = v
 mlab.mesh(x, y, z,
-      # color=np.exp(-np.linspace(-8, 8, 1000)**2/10),
-      colormap='Blues',
-      resolution=20,
-      representation='surface',
-      line_width=0,
-      # tube_radius=0,
-      # tube_sides=10,
-      opacity=0.9)
-
+      # color = np.exp(-np.linspace(-8, 8, 1000)**2/10),
+      colormap = 'Blues',
+      resolution = 20,
+      representation = 'surface',
+      line_width = 0,
+      # tube_radius = 0,
+      # tube_sides = 10,
+      opacity = 0.9
+)
 # mlab.savefig('mlaser.png')
 mlab.show()
